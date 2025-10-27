dc3ae25050d66d1a697c9533edcd959ea3c5baef
Mareola-Mabs <bulun1001@gmail.com>
2025-10-27 14:46:26 +0100

    feat: code the add task feature

--- src/tasks.ts
+++ src/tasks.ts
@@ -8,4 +8,55 @@ const btn1 = document.querySelector<HTMLButtonElement>('.test-btn')! // btn.disa
 
 // const btn = document.querySelector('.test-btn')! // Using exclamation(non -null assertion) also works, but will crash at runtime if the element doesn't 
 
+// Project Start
+const btn:HTMLButtonElement = btn1 // Renaming for use
+const taskForm = document.querySelector<HTMLFormElement>('form')!
+const formInput = document.querySelector<HTMLInputElement>('.form-input')!
+const taskListElement = document.querySelector<HTMLUListElement>('.list')!
 
+type Task = {
+    description: string,
+    isCompleted: boolean
+}
+
+const tasks: Task[] = []
+
+// Testing with Callbacks
+// function createTask(event: SubmitEvent){
+//         event.preventDefault()
+
+//     const taskDescription = formInput.value
+//     if(taskDescription){
+//         return formInput.value = ''
+//     }
+//     alert('Please enter a task description')
+// }
+
+// taskForm.addEventListener('submit', createTask)
+
+taskForm.addEventListener('submit', (event)=>{
+    event.preventDefault()
+
+    const taskDescription = formInput.value
+    if(taskDescription){
+        const task: Task = {
+                description: taskDescription,
+                isCompleted: false
+        }
+        // Add task to List
+        addTask(task)
+
+        // Render tasks
+
+        // Update local storage
+
+
+        return formInput.value = ''
+    }
+    alert('Please enter a task description')
+})
+
+function addTask(task: Task): void{
+    tasks.push(task)
+    console.log(tasks)
+}
